a630e2239fa43808a39b19d4665c9d8a8081e715
jsoyer <jsoyer@0cfc738c-9cf3-0310-86a7-d511f51c545f>
2008-09-12 07:28:56 +0000

    Ajout de la génération des certifs pour rsyslog
    git-svn-id: https://dev-eole.ac-dijon.fr/Eole2.2/amon/conf-amon/trunk@19857 0cfc738c-9cf3-0310-86a7-d511f51c545f

--- eole/amon.sh
+++ eole/amon.sh
@@ -54,3 +54,30 @@ then
 	usermod -s /usr/share/eole/manage-amon.sh amon
 fi
 echo
+if [ "$activer_log_distant" == "oui" -o "$activate_tls" == "oui" ]; then
+                if [ -e /etc/ssl/ca/ca-eole-rsyslog.pem ]; then
+                        echo "Génération de la clé..."
+
+                        /usr/bin/certtool --template /etc/ssl/eole-client-template.cnf --generate-privkey --outfile /etc/ssl/private/$nom_machine-key.pem --bits 2048
+
+                        echo "Génération de la requête de signature..."
+
+                        if [ ! -d /etc/ssl/request ]; then
+                                mkdir -p /etc/ssl/request
+                        fi
+
+                        /usr/bin/certtool --template /etc/ssl/eole-client-template.cnf --generate-request --load-privkey /etc/ssl/private/$nom_machine-key.pem --outfile /etc/ssl/request/$nom_machine-rsyslog-request.pem
+
+                        echo "Génération du couple clé/certificat signé..."
+
+                        if [ ! -d /etc/ssl/certs ]; then
+                                mkdir -p /etc/ssl/certs
+                        fi
+
+                        /usr/bin/certtool --template /etc/ssl/eole-client-template.cnf --generate-certificate --load-request /etc/ssl/request/$nom_machine-rsyslog-request.pem --outfile /etc/ssl/certs/$nom_machine-rsyslog-cert.pem --load-ca-certificate /etc/ssl/ca/ca-eole-rsyslog.pem --load-ca-privkey /etc/ssl/private/ca-eole-rsyslog-key.pem
+
+                else
+                        echo "Fichier CA de rsyslog non trouvé, merci de le transférer dans le répertoire /etc/ssl/ca"
+                fi
+fi
+
