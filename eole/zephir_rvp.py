9bd00792658e037b2920a7e2261ad70caca5557d
bboiget <bboiget@0cfc738c-9cf3-0310-86a7-d511f51c545f>
2008-11-28 10:30:49 +0000

    amon post_upgrade : correction si enregistrement d'un serveur non instancie
    git-svn-id: https://dev-eole.ac-dijon.fr/Eole2.2/amon/conf-amon/trunk@22228 0cfc738c-9cf3-0310-86a7-d511f51c545f

--- eole/zephir_rvp.py
+++ eole/zephir_rvp.py
@@ -11,11 +11,11 @@ try:
     id_sphynx=sys.argv[3]
     path=sys.argv[4]
     try:
-        zephir=xmlrpclib.ServerProxy("https://%s:%s@%s:7080" % (login,passwd,adresse_zephir), transport=TransportEole())
+        zephir_proxy = xmlrpclib.ServerProxy("https://%s:%s@%s:7080" % (login,passwd,adresse_zephir), transport=TransportEole())
     except xmlrpclib.ProtocolError:
         sys.exit("Erreur d'authentification zephir")
     # récupération de l'archive de configuration vpn
-    contenu_b64 = zephir.uucp.sphynx_get(id_sphynx,id_serveur)
+    contenu_b64 = zephir_proxy.uucp.sphynx_get(id_sphynx,id_serveur)
     contenu = xmlrpclib.base64.decodestring(contenu_b64[1])
     # écriture du fichier tar
     archive = path+os.sep+'vpn_%star.gz' % id_sphynx
