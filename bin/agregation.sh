47725787f8c2779673f80b33bcc86711e2a77515
smorin <samuel.morin@ac-dijon.fr>
2010-04-20 15:59:37 +0200

    fixes #444 : mise à jour du script d'agregation

--- bin/agregation.sh
+++ bin/agregation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ##############################
 # Script Agregation de lien  #
-# Gabriel CORGNE (Ac-Nantes) #
+# Gabriel CORGNE             #
 ##############################
 
 # Ajout marquage de packet pour SNAT
@@ -25,7 +25,7 @@ LHS2=1
 # Etat actuel du host
 CHS1=1
 CHS2=1
-# Le lien doit changer
+# Le lien doit changer (O:oui , 1:non)
 CLS1=1
 CLS2=1
 # Nombre de changement d'etat
@@ -72,7 +72,80 @@ ipruleclear () {
 		do ip rule del `echo $r|sed "s/-/ /g"`
 	done
 }
-		
+
+#Vidage des règles iptables de SNAT
+iptablessnatclear () {
+	i=1
+	while [ $i -le `/sbin/iptables -t nat -S POSTROUTING|wc -l` ]
+	do
+		if [ -n "`/sbin/iptables -t nat -S POSTROUTING $i|grep SNAT|grep "o eth0"`" ];then
+			/sbin/iptables -t nat -D POSTROUTING $i
+		else
+			let i++
+		fi
+	done
+}
+
+#Vidage des règles iptables de MANGLE $1 = nom de la carte
+iptablesmangleclear () {
+	i=1
+	while [ $i -le `/sbin/iptables -t mangle -S PREROUTING|wc -l` ]
+	do
+		if [ -n "`/sbin/iptables -t mangle -S PREROUTING $i|grep "i $1"`" ];then
+			/sbin/iptables -t mangle -D PREROUTING $i
+		else
+			let i++
+		fi
+	done
+}
+
+#Definition de la repartition par interface $1 = nom de la carte
+balance () {
+	iptablesmangleclear $1
+	network="$(eval echo \$adresse_network_$1)/$(eval echo \$adresse_netmask_$1)"
+	#Si non NEW
+	/sbin/iptables -t mangle -A PREROUTING -i $1 -s $network -m state ! --state NEW -j RESTOREMARK
+	/sbin/iptables -t mangle -A PREROUTING -i $1 -s $network -m state ! --state NEW -j RETURN
+	#Routes forcees
+	for ip_force1 in ${FORCE1[@]}; do
+		/sbin/iptables -t mangle -A PREROUTING -i $1 -s $network -d $ip_force1 -m state --state NEW -j T1
+		/sbin/iptables -t mangle -A PREROUTING -i $1 -s $network -d $ip_force1 -m state --state NEW -j RETURN ; done
+	for ip_force2 in ${FORCE2[@]}; do
+		/sbin/iptables -t mangle -A PREROUTING -i $1 -s $network -d $ip_force2 -m state --state NEW -j T2
+		/sbin/iptables -t mangle -A PREROUTING -i $1 -s $network -d $ip_force2 -m state --state NEW -j RETURN ; done
+	#Si NEW et recent alors Tag puis RETURN
+	/sbin/iptables -t mangle -A PREROUTING -i $1 -s $network -m state --state NEW -m recent --name T1 --update --rdest --seconds 3600 -j T1
+	/sbin/iptables -t mangle -A PREROUTING -i $1 -s $network -m state --state NEW -m recent --name T1 --update --rdest --seconds 3600 -j RETURN
+	/sbin/iptables -t mangle -A PREROUTING -i $1 -s $network -m state --state NEW -m recent --name T2 --update --rdest --seconds 3600 -j T2
+	/sbin/iptables -t mangle -A PREROUTING -i $1 -s $network -m state --state NEW -m recent --name T2 --update --rdest --seconds 3600 -j RETURN
+	#Si NEW sans recent alors Tag puis RETURN
+	/sbin/iptables -t mangle -A PREROUTING -i $1 -s $network -m state --state NEW -j T2
+	/sbin/iptables -t mangle -A PREROUTING -i $1 -s $network -m state --state NEW -m statistic --mode random --probability 0.$WCO -m recent --name T2 --set --rdest -j RETURN
+	/sbin/iptables -t mangle -A PREROUTING -i $1 -s $network -m state --state NEW -m recent --name T1 --set --rdest -j T1
+}
+#Definition du flux par interface $1 = nom de la carte
+wan1 () {
+	iptablesmangleclear $1
+	network="$(eval echo \$adresse_network_$1)/$(eval echo \$adresse_netmask_$1)"
+	#Si non NEW
+	/sbin/iptables -t mangle -A PREROUTING -i $1 -s $network -m state ! --state NEW -j RESTOREMARK
+	/sbin/iptables -t mangle -A PREROUTING -i $1 -s $network -m state ! --state NEW -j RETURN
+	#Marques sur T1
+	/sbin/iptables -t mangle -A PREROUTING -i $1 -s $network -m state --state NEW -j T1
+}
+#Definition du flux par interface $1 = nom de la carte
+wan2 () {
+	iptablesmangleclear $1
+	network="$(eval echo \$adresse_network_$1)/$(eval echo \$adresse_netmask_$1)"
+	#Si non NEW
+	/sbin/iptables -t mangle -A PREROUTING -i $1 -s $network -m state ! --state NEW -j RESTOREMARK
+	/sbin/iptables -t mangle -A PREROUTING -i $1 -s $network -m state ! --state NEW -j RETURN
+	#Marques sur T2
+	/sbin/iptables -t mangle -A PREROUTING -i $1 -s $network -m state --state NEW -j T2
+}
+## Initialisation
+# Large recent table
+/sbin/modprobe ipt_recent ip_list_tot=4000
 # Declaration des tables iproute2
 cat > /etc/iproute2/rt_tables <<Eof
 # reserved values
@@ -81,8 +154,8 @@ cat > /etc/iproute2/rt_tables <<Eof
 253	default
 0	unspec
 # local
-200 T2
-201 T1
+2 T2
+1 T1
 Eof
 
 # Vidage du cache
@@ -110,23 +183,29 @@ for ip_dns1 in ${DNS1[@]}; do /sbin/ip route add $ip_dns1 via $GW1 table main ; 
 for ip_dns2 in ${DNS2[@]}; do /sbin/ip route add $ip_dns2 via $GW2 table main ; done
 
 /sbin/ip route add default scope global nexthop via $GW1 dev eth0 weight $W1 nexthop via $GW2 dev eth0 weight $W2
-		
-## creation de la chaine marquage pour agregation de lien
-/sbin/iptables -t mangle -N CONNMARK1
-/sbin/iptables -t mangle -A CONNMARK1 -d 10.0.0.0/8 -j RETURN
-/sbin/iptables -t mangle -A CONNMARK1 -d 172.16.0.0/12 -j RETURN
-/sbin/iptables -t mangle -A CONNMARK1 -d 192.168.0.0/16 -j RETURN
-/sbin/iptables -t mangle -A CONNMARK1 -d 161.48.0.0/19 -j RETURN
-/sbin/iptables -t mangle -A CONNMARK1 -j MARK --set-mark 1
-/sbin/iptables -t mangle -A CONNMARK1 -j CONNMARK --save-mark
 
-/sbin/iptables -t mangle -N CONNMARK2
-/sbin/iptables -t mangle -A CONNMARK2 -d 10.0.0.0/8 -j RETURN
-/sbin/iptables -t mangle -A CONNMARK2 -d 172.16.0.0/12 -j RETURN
-/sbin/iptables -t mangle -A CONNMARK2 -d 192.168.0.0/16 -j RETURN
-/sbin/iptables -t mangle -A CONNMARK2 -d 161.48.0.0/19 -j RETURN
-/sbin/iptables -t mangle -A CONNMARK2 -j MARK --set-mark 2
-/sbin/iptables -t mangle -A CONNMARK2 -j CONNMARK --save-mark
+# Vidage des chaines MANGLE
+/sbin/iptables -t mangle -F PREROUTING
+/sbin/iptables -t mangle -F T1
+/sbin/iptables -t mangle -F T2
+/sbin/iptables -t mangle -F RESTOREMARK
+
+## creation de la chaine marquage pour agregation de lien
+/sbin/iptables -t mangle -N T1
+/sbin/iptables -t mangle -A T1 -d 10.0.0.0/8 -j RETURN
+/sbin/iptables -t mangle -A T1 -d 172.16.0.0/12 -j RETURN
+/sbin/iptables -t mangle -A T1 -d 192.168.0.0/16 -j RETURN
+/sbin/iptables -t mangle -A T1 -d 161.48.0.0/19 -j RETURN
+/sbin/iptables -t mangle -A T1 -j MARK --set-mark 1
+/sbin/iptables -t mangle -A T1 -j CONNMARK --save-mark
+
+/sbin/iptables -t mangle -N T2
+/sbin/iptables -t mangle -A T2 -d 10.0.0.0/8 -j RETURN
+/sbin/iptables -t mangle -A T2 -d 172.16.0.0/12 -j RETURN
+/sbin/iptables -t mangle -A T2 -d 192.168.0.0/16 -j RETURN
+/sbin/iptables -t mangle -A T2 -d 161.48.0.0/19 -j RETURN
+/sbin/iptables -t mangle -A T2 -j MARK --set-mark 2
+/sbin/iptables -t mangle -A T2 -j CONNMARK --save-mark
 
 /sbin/iptables -t mangle -N RESTOREMARK
 /sbin/iptables -t mangle -A RESTOREMARK -d 10.0.0.0/8 -j RETURN
@@ -136,15 +215,7 @@ for ip_dns2 in ${DNS2[@]}; do /sbin/ip route add $ip_dns2 via $GW2 table main ; 
 /sbin/iptables -t mangle -A RESTOREMARK -j CONNMARK --restore-mark
 
 ## Mise a jour des règles de SNAT
-i=1
-while [ $i -le `/sbin/iptables -t nat -S POSTROUTING|wc -l` ]
-do
-        if [ -n "`/sbin/iptables -t nat -S POSTROUTING $i|grep SNAT|grep "o eth0"`" ];then
-                /sbin/iptables -t nat -D POSTROUTING $i
-        else
-                let i++
-        fi
-done
+iptablessnatclear
 /sbin/iptables -t nat -A POSTROUTING -o eth0 -m mark --mark 1 -j SNAT --to-source $WAN1
 /sbin/iptables -t nat -A POSTROUTING -o eth0 -m mark --mark 2 -j SNAT --to-source $WAN2
 
@@ -164,7 +235,7 @@ while [ $IM -lt $IM_C ] && [ $SUCCES -eq 1 ] ; do
 	while [ $ID -lt $(eval echo \$ID$L\_C) ] && [ $SUCCES -eq 1 ] ; do
 		host -W $TIMEOUT ${MIRE[$IM]} $(eval echo \${DNS$L[$ID]})> /dev/null  2>&1
 		if [ $? -ne 0 ]; then
-			[ $(eval echo \$LLS$L) -eq 0 ] && Aecho "Erreur de resolution de $(eval echo \${MIRE[$IM]}) sur le dns $(eval echo \${DNS$L[$ID]}) du lien $L" 
+			[ $(eval echo \$LLS$L) -eq 0 ] && Aecho "Erreur de resolution de $(eval echo \${MIRE[$IM]}) sur le dns $(eval echo \${DNS$L[$ID]}) du lien $L"
 			ID=$(( $ID + 1 ))
 		else
 			SUCCES=0
@@ -172,8 +243,8 @@ while [ $IM -lt $IM_C ] && [ $SUCCES -eq 1 ] ; do
 	done
 	IM=$(( $IM + 1 ))
 	ID=0
-done		
-		
+done
+
 	if [ $SUCCES -eq 1 ]; then
 		Zecho "Le lien $L est tombe"
 		eval CHS$L=1
@@ -197,7 +268,7 @@ done
 		else
 			eval LLS$L=1
 		fi
-	else 
+	else
 		eval CLS$L=1
 	fi
 
@@ -213,71 +284,39 @@ while : ; do
 	Checkstate 2
 
 	if [[ $CLS1 -eq 0 || $CLS2 -eq 0 ]]; then
-		if [[ $LLS1 -eq 1 && $LLS2 -eq 0 ]]; then 
+		if [[ $LLS1 -eq 1 && $LLS2 -eq 0 ]]; then
 			Aecho "Seul le lien 2 est actif, redirection des flux sur ce lien"
 			# Iproute2 sur le lien2
-			/sbin/ip route replace default scope global via $GW2 dev eth0
+			/sbin/ip route replace default via $GW2 dev eth0
 			# Mangle sur le lien2
-			/sbin/iptables -t mangle -A PREROUTING -i eth1 -s $adresse_network_eth1/$adresse_netmask_eth1 -m state --state NEW -j CONNMARK2
-if [ $nombre_interfaces -ge 3 ];then
-			/sbin/iptables -t mangle -A PREROUTING -i eth2 -s $adresse_network_eth2/$adresse_netmask_eth2 -m state --state NEW -j CONNMARK2
-fi
-if [ $nombre_interfaces -ge 4 ];then
-			/sbin/iptables -t mangle -A PREROUTING -i eth3 -s $adresse_network_eth3/$adresse_netmask_eth3 -m state --state NEW -j CONNMARK2
-fi
-if [ $nombre_interfaces -eq 5 ];then
-			/sbin/iptables -t mangle -A PREROUTING -i eth4 -s $adresse_network_eth4/$adresse_netmask_eth4 -m state --state NEW -j CONNMARK2
-fi
+			wan2 eth1
+			[ $nombre_interfaces -ge 3 ] && wan2 eth2
+			[ $nombre_interfaces -ge 4 ] && wan2 eth3
+			[ $nombre_interfaces -eq 5 ] && wan2 eth4
 		elif [[ $LLS1 -eq 0 && $LLS2 -eq 1 ]]; then
 			Aecho "Seul le lien 1 est actif, redirection des flux sur ce lien"
 			# Iproute2 sur le lien1
-			/sbin/ip route replace default scope global via $GW1 dev eth0
+			#/sbin/ip route replace default scope global via $GW1 dev eth0
+			/sbin/ip route replace default via $GW1 dev eth0
 			# Mangle sur lien1
-			/sbin/iptables -t mangle -A PREROUTING -i eth1 -s $adresse_network_eth1/$adresse_netmask_eth1 -m state --state NEW -j CONNMARK1
-if [ $nombre_interfaces -ge 3 ];then
-			/sbin/iptables -t mangle -A PREROUTING -i eth2 -s $adresse_network_eth2/$adresse_netmask_eth2 -m state --state NEW -j CONNMARK1
-fi
-if [ $nombre_interfaces -ge 4 ];then
-			/sbin/iptables -t mangle -A PREROUTING -i eth3 -s $adresse_network_eth3/$adresse_netmask_eth3 -m state --state NEW -j CONNMARK1
-fi
-if [ $nombre_interfaces -eq 5 ];then
-			/sbin/iptables -t mangle -A PREROUTING -i eth4 -s $adresse_network_eth4/$adresse_netmask_eth4 -m state --state NEW -j CONNMARK1
-fi
-			# rechargement des tunnels
-			/usr/share/eole/magic-rvp &
+			wan1 eth1
+			[ $nombre_interfaces -ge 3 ] && wan1 eth2
+			[ $nombre_interfaces -ge 4 ] && wan1 eth3
+			[ $nombre_interfaces -eq 5 ] && wan1 eth4
 		elif [[ $LLS1 -eq 0 && $LLS2 -eq 0 ]]; then
 			Aecho "Rechargement de la repartition sur les 2 liens"
 			# Iproute2 sur les 2 liens
-			/sbin/ip route replace default scope global nexthop via $GW1 dev eth0 weight $W1 nexthop via $GW2 dev eth0 weight $W2
+			#/sbin/ip route replace default scope global nexthop via $GW1 dev eth0 weight $W1 nexthop via $GW2 dev eth0 weight $W2
+			/sbin/ip route replace default proto static nexthop via $GW1 dev eth0 weight $W1 nexthop via $GW2 dev eth0 weight $W2
+			#/sbin/ip route replace default equalize nexthop via $GW1 dev eth0 weight $W1 nexthop via $GW2 dev eth0 weight $W2
 			# Mangle sur les 2 liens
-			/sbin/iptables -t mangle -A PREROUTING -i eth1 -s $adresse_network_eth1/$adresse_netmask_eth1 -m state --state NEW -j CONNMARK1
-			/sbin/iptables -t mangle -A PREROUTING -i eth1 -s $adresse_network_eth1/$adresse_netmask_eth1 -m state --state NEW -m statistic --mode random --probability 0.$WCO -j CONNMARK2
-if [ $nombre_interfaces -ge 3 ];then
-			/sbin/iptables -t mangle -A PREROUTING -i eth2 -s $adresse_network_eth2/$adresse_netmask_eth2 -m state --state NEW -j CONNMARK1
-			/sbin/iptables -t mangle -A PREROUTING -i eth2 -s $adresse_network_eth2/$adresse_netmask_eth2 -m state --state NEW -m statistic --mode random --probability 0.$WCO -j CONNMARK2
-fi
-if [ $nombre_interfaces -ge 4 ];then
-			/sbin/iptables -t mangle -A PREROUTING -i eth3 -s $adresse_network_eth3/$adresse_netmask_eth3 -m state --state NEW -j CONNMARK1
-			/sbin/iptables -t mangle -A PREROUTING -i eth3 -s $adresse_network_eth3/$adresse_netmask_eth3 -m state --state NEW -m statistic --mode random --probability 0.$WCO -j CONNMARK2
-fi
-if [ $nombre_interfaces -eq 5 ];then
-			/sbin/iptables -t mangle -A PREROUTING -i eth4 -s $adresse_network_eth4/$adresse_netmask_eth4 -m state --state NEW -j CONNMARK1
-			/sbin/iptables -t mangle -A PREROUTING -i eth4 -s $adresse_network_eth4/$adresse_netmask_eth4 -m state --state NEW -m statistic --mode random --probability 0.$WCO -j CONNMARK2
-fi
-			# rechargement des tunnels
-			/usr/share/eole/magic-rvp &
+			balance eth1
+			[ $nombre_interfaces -ge 3 ] && balance eth2
+			[ $nombre_interfaces -ge 4 ] && wan1 eth3
+			[ $nombre_interfaces -eq 5 ] && wan2 eth4
 		fi
-		# Restauration des marques
-		/sbin/iptables -t mangle -A PREROUTING -i eth1 -s $adresse_network_eth1/$adresse_netmask_eth1 -j RESTOREMARK
-if [ $nombre_interfaces -ge 3 ];then
-		/sbin/iptables -t mangle -A PREROUTING -i eth2 -s $adresse_network_eth2/$adresse_netmask_eth2 -j RESTOREMARK
-fi
-if [ $nombre_interfaces -ge 4 ];then
-		/sbin/iptables -t mangle -A PREROUTING -i eth3 -s $adresse_network_eth3/$adresse_netmask_eth3 -j RESTOREMARK
-fi
-if [ $nombre_interfaces -eq 5 ];then
-		/sbin/iptables -t mangle -A PREROUTING -i eth4 -s $adresse_network_eth4/$adresse_netmask_eth4 -j RESTOREMARK
-fi
+		# rechargement des tunnels
+		/usr/share/eole/magic-rvp &
 	fi
 
     if [ $PAUSE -le 5 ];then
