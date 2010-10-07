2689a3bea5285d21bf56da281d58cad63fb21d7c
Joël Cuissinat <joel.cuissinat@ac-dijon.fr>
2010-10-07 15:00:42 +0200

    amélioration du script enregistrement_domaine.sh [2.3] (fixes #992)

--- proxy/eole/enregistrement_domaine.sh
+++ proxy/eole/enregistrement_domaine.sh
@@ -7,16 +7,18 @@
 . /etc/eole/containers.conf
 . /usr/share/eole/FonctionsEoleNg
 
+RunCmd "/usr/bin/wbinfo -t" proxy &>/dev/null
+if [ $? -eq 0 ];then
+    QUESTION="Le serveur est déjà intégré à un domaine\nRelancer l'intégration ?"
+    Question_ouinon $QUESTION non warn
+    [ $? -ne 0 ] && exit 0
+fi
+
 #redemarrage de samba
 echo "*** Redémarrage des services pour l'enregistrement au domaine ***"
 Service winbind stop proxy &>/dev/null
 Service smbd restart proxy &>/dev/null
 Service winbind start proxy &>/dev/null
-
-if [ -e "$container_path_proxy/var/run/samba/winbindd_privileged" ]; then
-    RunCmd "chgrp proxy /var/run/samba/winbindd_privileged" proxy
-    Service squid restart proxy
-fi
 
 #inscription de la station dans un domaine
 echo
@@ -24,12 +26,11 @@ echo "Entrer le nom de l'administrateur du serveur Windows :"
 read user_admin
 echo "Entrer le mot de passe de l'administrateur du serveur Windows :"
 read -s mdp_admin
-RunCmd "/usr/bin/net ads join -U $user_admin%$mdp_admin" proxy #&>dev/null
-
-#test de l'intégration
+RunCmd "/usr/bin/net ads join -U $user_admin%$mdp_admin" proxy
 echo
 
-RunCmd "/usr/bin/wbinfo -t" proxy #&> /dev/null
+#test de l'intégration
+RunCmd "/usr/bin/wbinfo -t" proxy &>/dev/null
 if [ $? -eq 1 ]; then
     EchoRouge "L'intégration au domaine a échoué"
 else
