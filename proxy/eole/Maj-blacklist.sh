a1ca0d77c6011cd846f5dbe45b443197df599283
Joël Cuissinat <joel.cuissinat@ac-dijon.fr>
2011-04-21 17:27:12 +0200

    mise à niveau de Maj-blacklist.sh pour #1724

--- proxy/eole/Maj-blacklist.sh
+++ proxy/eole/Maj-blacklist.sh
@@ -28,7 +28,7 @@ if [ -n "$http_proxy" ];then
     TestService "Serveur Proxy" $Proxy 2>&1
     if [ $? != 0 ];then
         Zephir "ERR" "Le proxy $Proxy ne répond pas !" "Maj-blacklist" 2>&1 | tee -a $F_LOG
-	#sed -i 1i"Erreur : impossible d'accéder au site" $F_LOG
+        #sed -i 1i"Erreur : impossible d'accéder au site" $F_LOG
         exit 1
     fi
 fi
@@ -46,94 +46,91 @@ echo "Téléchargement des bases"
 
 res=`wget --timestamping $url_maj_blacklist/blacklists.tar.gz 2>&1`
 if [ $? == 1 ];then
-	echo "Le fichier blacklists.tar.gz n'a pas été trouvé !" | tee -a $F_LOG
-	sed -i 1i"Erreur : blacklists.tar.gz non disponible" $F_LOG
-	exit 1
+    echo "Le fichier blacklists.tar.gz n'a pas été trouvé !" | tee -a $F_LOG
+    sed -i 1i"Erreur : blacklists.tar.gz non disponible" $F_LOG
+    exit 1
 fi
 echo "$res" | grep -q -E "non récupéré|not retrieving"
 if [ $? == 0 ];then
-	blacklists="0"
+    blacklists="0"
 else
-	blacklists="1"
+    blacklists="1"
 fi
 
 ## Fichier weighted ##
 res=`wget --timestamping $url_maj_blacklist/weighted 2>&1`
 if [ $? == 1 ];then
-	echo "Le fichier weighted n'a pas été trouvé !" | tee -a $F_LOG
-	sed -i 1i"Erreur : fichier weighted non disponible" $F_LOG
-	exit 1
+    echo "Le fichier weighted n'a pas été trouvé !" | tee -a $F_LOG
+    sed -i 1i"Erreur : fichier weighted non disponible" $F_LOG
+    exit 1
 fi
 echo "$res" | grep -q -E "non récupéré|not retrieving"
 if [ $? == 0 ];then
-	weighted="0"
+    weighted="0"
 else
-	weighted="1"
+    weighted="1"
 fi
 
 ## Base blacklists (si nécessaire) ##
 if [ "$blacklists" == "1" ];then
-	echo "Intégration des bases"
-	tar -xzf blacklists.tar.gz
-	if [ $? -ne 0 ];then
-		echo "L'archive blacklists.tar.gz n'a pas pu être décompressée !" | tee -a $F_LOG
-		sed -i 1i"Erreur : décompression de blacklists.tar.gz impossible" $F_LOG
-		exit 1
-	fi
+    echo "Intégration des bases"
+    tar -xzf blacklists.tar.gz
+    if [ $? -ne 0 ];then
+        echo "L'archive blacklists.tar.gz n'a pas pu être décompressée !" | tee -a $F_LOG
+        sed -i 1i"Erreur : décompression de blacklists.tar.gz impossible" $F_LOG
+        exit 1
+    fi
 
-	## Filtres obligatoires (dans db) ##
-	for base in "adult" "redirector"
-	do
-		[ ! -d $DB_PATH/db/$base ] && mkdir -p $DB_PATH/db/$base
-		for file in "$DB_PATH/tmp/blacklists/$base/domains" "$DB_PATH/tmp/blacklists/$base/urls"
-		do
-			if [ -f $file ];then
-				count=`wc -l $file | awk -F " " '{print $1}'`
-				if [ ! "$count" -eq "0" ];then
-					cp $file $DB_PATH/db/$base
-				else
-					echo 'le fichier' $file 'est vide !'
-				fi
-			fi
-		done
-	done
+    ## Filtres obligatoires (dans db) ##
+    for base in "adult" "redirector"
+    do
+        [ ! -d $DB_PATH/db/$base ] && mkdir -p $DB_PATH/db/$base
+        for file in "$DB_PATH/tmp/blacklists/$base/domains" "$DB_PATH/tmp/blacklists/$base/urls"
+        do
+            if [ -f $file ];then
+                count=`wc -l $file | awk -F " " '{print $1}'`
+                if [ ! "$count" -eq "0" ];then
+                    cp $file $DB_PATH/db/$base
+                else
+                    echo 'le fichier' $file 'est vide !'
+                fi
+            fi
+        done
+    done
+    RunCmd "chown -R proxy.proxy $SHORT_DB_PATH/db/"   proxy
 
-	## Filtres optionnels (dans eole) ##
-	# FIXME : concaténation de bases ?
-	for base in "agressif" "audio-video" "drogue" "forums" "gambling" "games" "hacking" "mobile-phone" "phishing" \
-		    "publicite" "radio" "tricheur" "warez" "webmail" "strict_redirector" "strong_redirector" "mixed_adult"
-	do
-		[ ! -d $DB_PATH/eole/$base ] && mkdir -p $DB_PATH/eole/$base
-		[ ! -d $DB_PATH/db/$base ] && mkdir -p $DB_PATH/db/$base ## Desuet n'est plus utilise dans dansguardian?
-		[ ! -e $DB_PATH/eole/$base/domains ] && touch $DB_PATH/eole/$base/domains
-		[ ! -e $DB_PATH/eole/$base/urls ] && touch $DB_PATH/eole/$base/urls
+    ## Filtres optionnels (dans eole) ##
+    for base in `cut -d '#' -f1 /usr/share/ead2/backend/config/filtres-opt`
+    do
+        [ ! -d $DB_PATH/eole/$base ] && mkdir -p $DB_PATH/eole/$base
+        [ ! -e $DB_PATH/eole/$base/domains ] && touch $DB_PATH/eole/$base/domains
+        [ ! -e $DB_PATH/eole/$base/urls ] && touch $DB_PATH/eole/$base/urls
 
-		for file in "$DB_PATH/tmp/blacklists/$base/domains" "$DB_PATH/tmp/blacklists/$base/urls"
-		do
-			if [ -f $file ];then
-				count=`wc -l $file | awk -F " " '{print $1}'`
-				if [ ! "$count" -eq "0" ];then
-					cp $file $DB_PATH/eole/$base
-				else
-					echo 'le fichier' $file 'est vide !'
-				fi
-			fi
-		done
-	done
+        for file in "$DB_PATH/tmp/blacklists/$base/domains" "$DB_PATH/tmp/blacklists/$base/urls"
+        do
+            if [ -f $file ];then
+                count=`wc -l $file | awk -F " " '{print $1}'`
+                if [ ! "$count" -eq "0" ];then
+                    cp $file $DB_PATH/eole/$base
+                else
+                    echo 'le fichier' $file 'est vide !'
+                fi
+            fi
+        done
+    done
+    RunCmd "chown -R proxy.proxy $SHORT_DB_PATH/eole/" proxy
 
-	RunCmd "chown -R proxy.proxy $SHORT_DB_PATH/eole/" proxy
-	RunCmd "chown -R proxy.proxy $SHORT_DB_PATH/db/"   proxy
 else
-	echo "Rien à faire pour blacklists.tar.gz"
+    echo "Rien à faire pour blacklists.tar.gz"
 fi
 
 ## Fichier weighted (si nécessaire) ##
 if [ $weighted == "1" ]
 then
-	echo "Copie du fichier weighted"
-	cp -f $DB_PATH/tmp/weighted $META_PATH
+    echo "Copie du fichier weighted"
+    cp -f $DB_PATH/tmp/weighted $META_PATH
 else
-	echo "Rien à faire pour le fichier weighted"
+    echo "Rien à faire pour le fichier weighted"
 fi
 
 
@@ -156,5 +153,5 @@ rm -rf $DB_PATH/tmp/blacklists
 
 # redémarrage si au moins une modification
 if [ "$blacklists" == "1" -o $weighted == "1" ];then
-	Service dansguardian restart proxy
+        Service dansguardian restart proxy
 fi
